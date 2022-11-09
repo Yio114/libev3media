2479d35baa407f4efe9d57c9c1095f3dbc05c278
Grigoriy Bulygin <grigoriy.bulygin@gmail.com>
2022-11-09 20:02:12 +0300

    Немного форматирования

--- audio.cpp
+++ audio.cpp
@@ -8,7 +8,9 @@
 #define P_ERR(err, func) if((err = func) < 0) {std::cout << snd_strerror(err) << std::endl; return;}
 
 namespace ev3media {
+	//////////////////////////////////
 	// sound
+	//////////////////////////////////
 
 	sound::sound() {
 		available = false;
@@ -53,7 +55,7 @@ namespace ev3media {
 			src_format, src_samplerate, src_channels);
 	}
 
-    //FIX ME
+	//FIX ME
 	void sound::convert(const uint8_t* data, size_t size, int format, int rate, int channels) {
 		if(format == 16 && rate == 22050 && channels == 1) {
 			sound_data = std::shared_ptr<uint8_t>(new uint8_t[size]);
@@ -69,15 +71,21 @@ namespace ev3media {
 	}
 	
 	
-    size_t sound::get_data_size() {
-        return sound_size;
-    }
-    
-    std::shared_ptr<uint8_t> sound::get_data() {
-        return sound_data;
-    }
+	size_t sound::get_data_size() {
+		return sound_size;
+	}
+	
+	std::shared_ptr<uint8_t> sound::get_data() {
+		return sound_data;
+	}
 
+
+
+
+
+	//////////////////////////////////
 	// audio_player
+	//////////////////////////////////
 
 	void audio_player::init(uint32_t max_channels) {
 		sample_rate = 22050;
@@ -90,33 +98,33 @@ namespace ev3media {
 		async_handler_thread.detach();
 	}
 
-   	void audio_player::init_alsa() {
-      		available = false;
-        	snd_pcm_hw_params_t* hw_params;
-		    int error = 0;
+	void audio_player::init_alsa() {
+		available = false;
+		snd_pcm_hw_params_t* hw_params;
+		int error = 0;
 
-        	P_ERR(error, snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, 0))
-        	P_ERR(error, snd_pcm_hw_params_malloc(&hw_params))
-        	P_ERR(error, snd_pcm_hw_params_any(pcm_handle, hw_params))
-        	P_ERR(error, snd_pcm_hw_params_set_rate_resample(pcm_handle, hw_params, 1))
-        	P_ERR(error, snd_pcm_hw_params_set_access(pcm_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED))
-        	P_ERR(error, snd_pcm_hw_params_set_format(pcm_handle, hw_params, SND_PCM_FORMAT_S16_LE))
-        	P_ERR(error, snd_pcm_hw_params_set_channels(pcm_handle, hw_params, 1))
-        	P_ERR(error, snd_pcm_hw_params_set_rate_near(pcm_handle, hw_params, &sample_rate, 0))
-        	P_ERR(error, snd_pcm_hw_params_set_period_size(pcm_handle, hw_params, 4096/*4096 * 4*/, 0))
-        	P_ERR(error, snd_pcm_hw_params(pcm_handle, hw_params))
+		P_ERR(error, snd_pcm_open(&pcm_handle, "default", SND_PCM_STREAM_PLAYBACK, 0))
+		P_ERR(error, snd_pcm_hw_params_malloc(&hw_params))
+		P_ERR(error, snd_pcm_hw_params_any(pcm_handle, hw_params))
+		P_ERR(error, snd_pcm_hw_params_set_rate_resample(pcm_handle, hw_params, 1))
+		P_ERR(error, snd_pcm_hw_params_set_access(pcm_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED))
+		P_ERR(error, snd_pcm_hw_params_set_format(pcm_handle, hw_params, SND_PCM_FORMAT_S16_LE))
+		P_ERR(error, snd_pcm_hw_params_set_channels(pcm_handle, hw_params, 1))
+		P_ERR(error, snd_pcm_hw_params_set_rate_near(pcm_handle, hw_params, &sample_rate, 0))
+		P_ERR(error, snd_pcm_hw_params_set_period_size(pcm_handle, hw_params, 4096/*4096 * 4*/, 0))
+		P_ERR(error, snd_pcm_hw_params(pcm_handle, hw_params))
 
-        	snd_pcm_uframes_t pcm_buffer_size = 0;
-        	snd_pcm_hw_params_get_buffer_size(hw_params, &pcm_buffer_size);
+		snd_pcm_uframes_t pcm_buffer_size = 0;
+		snd_pcm_hw_params_get_buffer_size(hw_params, &pcm_buffer_size);
 
-        	snd_pcm_uframes_t pcm_period_size = 0;
-        	snd_pcm_hw_params_get_period_size(hw_params, &pcm_period_size, 0);
+		snd_pcm_uframes_t pcm_period_size = 0;
+		snd_pcm_hw_params_get_period_size(hw_params, &pcm_period_size, 0);
 
-		    snd_pcm_hw_params_free(hw_params);
+		snd_pcm_hw_params_free(hw_params);
 
-        	period_size = pcm_period_size;
-        	buffer_size = pcm_buffer_size;
-        	available = true;
+		period_size = pcm_period_size;
+		buffer_size = pcm_buffer_size;
+		available = true;
 	}
 
 	audio_player::audio_player() {
@@ -127,64 +135,64 @@ namespace ev3media {
 		init(max_channels);
 	}
 	
-    void audio_player::play(sound* snd, uint32_t channel) {
-        if(snd != nullptr) {
-            if(snd->is_available()) {
-                channel_t new_data;
-                memset(&new_data, 0, sizeof(channel_t));
+	void audio_player::play(sound* snd, uint32_t channel) {
+		if(snd != nullptr) {
+			if(snd->is_available()) {
+				channel_t new_data;
+				memset(&new_data, 0, sizeof(channel_t));
 
-                new_data.len = snd->get_data_size();
-                new_data.data = snd->get_data();
-                new_data.playing = true;
-                new_data.volume = 100;
-                channels[channel] = new_data;
-            }
-        }
-    }
-    
-    bool audio_player::is_playing(uint32_t channel) {
-        if(channel < channels_count) {
-            return channels[channel].playing && (channels[channel].pos < channels[channel].len) && available;
-        }
-        else {
-            return false;
-        }
-    }
-        
-    void audio_player::pause_channel(uint32_t c, bool paused) {
-        if(c < channels_count)
-            channels[c].playing = !paused;
-    }
-    
-    void audio_player::pause_all(bool paused) {
-        for(uint32_t i = 0; i < channels_count; i++) {
-            channels[i].playing = !paused;
-        }
-    }
+				new_data.len = snd->get_data_size();
+				new_data.data = snd->get_data();
+				new_data.playing = true;
+				new_data.volume = 100;
+				channels[channel] = new_data;
+			}
+		}
+	}
+	
+	bool audio_player::is_playing(uint32_t channel) {
+		if(channel < channels_count) {
+			return channels[channel].playing && (channels[channel].pos < channels[channel].len) && available;
+		}
+		else {
+			return false;
+		}
+	}
+		
+	void audio_player::pause_channel(uint32_t c, bool paused) {
+		if(c < channels_count)
+			channels[c].playing = !paused;
+	}
+	
+	void audio_player::pause_all(bool paused) {
+		for(uint32_t i = 0; i < channels_count; i++) {
+			channels[i].playing = !paused;
+		}
+	}
 
-    void audio_player::clear_channel(uint32_t channel) {
-        memset(&channels[channel], 0, sizeof(channel_t));
-    }
-    
-    void audio_player::clear_all() {
-        memset(channels, 0, sizeof(channel_t) * channels_count);
-    }
+	void audio_player::clear_channel(uint32_t channel) {
+		memset(&channels[channel], 0, sizeof(channel_t));
+	}
+	
+	void audio_player::clear_all() {
+		memset(channels, 0, sizeof(channel_t) * channels_count);
+	}
 
-    void audio_player::volume_channel(uint32_t channel, float volume) {
-        if(channel < channels_count)
-            channels[channel].volume = (uint8_t)(volume * 100);
-    }
-    
-    void audio_player::volume_all(float volume) {
-        uint8_t formatted_volume = (uint8_t)(volume * 100);
-        for(uint32_t i = 0; i < channels_count; i++) {
-            channels[i].volume = formatted_volume;
-        }
-    }
-    
-    bool audio_player::is_available() {
-        return available;
-    }
+	void audio_player::volume_channel(uint32_t channel, float volume) {
+		if(channel < channels_count)
+			channels[channel].volume = (uint8_t)(volume * 100);
+	}
+	
+	void audio_player::volume_all(float volume) {
+		uint8_t formatted_volume = (uint8_t)(volume * 100);
+		for(uint32_t i = 0; i < channels_count; i++) {
+			channels[i].volume = formatted_volume;
+		}
+	}
+	
+	bool audio_player::is_available() {
+		return available;
+	}
 	
 
 	void audio_player::audio_player::free() {
@@ -192,7 +200,7 @@ namespace ev3media {
 		if(available && pcm_handle != nullptr) {
 			snd_pcm_close(pcm_handle);
 		}
-        
+		
 		delete[] channels;
 	}
 
@@ -207,43 +215,43 @@ namespace ev3media {
 	}
 
 	void audio_player::async_handler(audio_player* instance, bool** running_ptr) {
-	    bool running = true;
-	    *running_ptr = &running;
+		bool running = true;
+		*running_ptr = &running;
 
-	    uint32_t buffer_size = instance->period_size * 2;
-    	std::unique_ptr<uint8_t> buffer = std::unique_ptr<uint8_t>(new uint8_t[buffer_size]);
+		uint32_t buffer_size = instance->period_size * 2;
+		std::unique_ptr<uint8_t> buffer = std::unique_ptr<uint8_t>(new uint8_t[buffer_size]);
 
-    	snd_pcm_t* pcm = instance->pcm_handle;
-	    int err = 0;
+		snd_pcm_t* pcm = instance->pcm_handle;
+		int err = 0;
 
-    	while(running) {
-    		memset(buffer.get(), 0, buffer_size);
-    		for(int i = 0; i < instance->channels_count; i++) {
-                channel_t* channel = &instance->channels[i];
-                
-                if(channel->pos < channel->len && channel->playing) {
-                    uint32_t len = channel->len - channel->pos;
-                    
-                    if(len > buffer_size) {
-	                    len = buffer_size;
-	                }
+		while(running) {
+			memset(buffer.get(), 0, buffer_size);
+			for(int i = 0; i < instance->channels_count; i++) {
+				channel_t* channel = &instance->channels[i];
+				
+				if(channel->pos < channel->len && channel->playing) {
+					uint32_t len = channel->len - channel->pos;
+					
+					if(len > buffer_size) {
+						len = buffer_size;
+					}
 
-                    mix_audio(buffer.get(), &channel->data.get()[channel->pos], len, channel->volume);
-                    channel->pos += len;
-                }
-            }
+					mix_audio(buffer.get(), &channel->data.get()[channel->pos], len, channel->volume);
+					channel->pos += len;
+				}
+			}
 
-    		if(running) {
-        		if((err = snd_pcm_writei(pcm, buffer.get(), instance->period_size)) < 0) {
-         	   		if(snd_pcm_recover(pcm, err, 1) < 0) {
-           	     			running = false;
-	                }
-                }
-                if(snd_pcm_avail(pcm) + instance->period_size < instance->buffer_size) {
-                    		usleep(instance->period_size * 1000000 / instance->sample_rate);
-                }
-        	}
-	    }
+			if(running) {
+				if((err = snd_pcm_writei(pcm, buffer.get(), instance->period_size)) < 0) {
+					if(snd_pcm_recover(pcm, err, 1) < 0) {
+						running = false;
+					}
+				}
+				if(snd_pcm_avail(pcm) + instance->period_size < instance->buffer_size) {
+					sleep(instance->period_size * 1000000 / instance->sample_rate);
+				}
+			}
+		}
 	}
 }
 
